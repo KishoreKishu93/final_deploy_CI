44e1813436a641dbc94cac6e7f4b17fcdaca9eac
Kishore Kumar V <kishore.kumarkv93@gmail.com>
2023-08-09 11:51:40 +0530

    Docker updated

--- app.py
+++ app.py
@@ -29,11 +29,12 @@ def predict():
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
-
+    return render_template("home.html", prediction_text="The House price prediction is {}".format(output))
+    
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
    
      
